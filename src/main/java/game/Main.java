7c0ac2d2dd16b744b0d3a76421805eb703e4c7a5
jrucl2d <ysk789465@gmail.com>
2023-06-30 02:12:00 +0900

    게임 프레임 띄우기, 정중앙에 나오도록 설정

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,6 +1,7 @@
 package game;
 
 
+import game.windows.components.FrameBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +9,12 @@ public class Main {
     final static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        logger.info("Game을 시작합니다.");
+        logger.info("오목과 지뢰찾기 게임을 시작합니다.");
+
+        FrameBuilder.builder()
+                .title("오목/지뢰찾기 게임")
+                .width(400)
+                .height(400)
+                .build();
     }
 }
--- src/main/java/game/windows/components/FrameBuilder.java
+++ src/main/java/game/windows/components/FrameBuilder.java
@@ -1,11 +1,16 @@
 package game.windows.components;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FrameBuilder {
     private String title;
     private int width;
     private int height;
+
+    public static FrameBuilder builder() {
+        return new FrameBuilder();
+    }
 
     public FrameBuilder title(final String title) {
         this.title = title;
@@ -22,12 +27,19 @@ public class FrameBuilder {
         return this;
     }
 
-    public Frame build() {
+    public void build() {
         final JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle(title);
         frame.setSize(width, height);
+
+        // 화면 가운데에 보이도록 설정
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screenSize.width - width) / 2;
+        int y = (screenSize.height - height) / 2;
+        frame.setLocation(x, y);
+
         frame.setVisible(true);
-        return new Frame(frame);
+        new Frame(frame);
     }
 }
